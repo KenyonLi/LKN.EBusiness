4a298f60fa4355337f1108a49805a95e677997c9
Kenyon Li <kenyonli@outlook.com>
2023-08-13 13:02:29 +0800

    -nginx

---
+++ LKN.EBusiness.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LKN.EBusiness.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
