f1cc4420d0f841f7464ecdbc034f85bc61c2f500
Kenyon Li <kenyonli@outlook.com>
2023-09-15 16:51:03 +0800

    -

---
+++ schedulemaster/microservice/LKN.ScheduleService/OrderCancelTask.cs
@@ -0,0 +1,18 @@
+﻿using Hos.ScheduleMaster.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LKN.ScheduleService
+{
+    public class OrderCancelTask : TaskBase
+    {
+        public override void Run(TaskContext context)
+        {
+            // 1、超时定时逻辑
+            context.WriteLog("回收超时订单......成功");
+        }
+    }
+}
